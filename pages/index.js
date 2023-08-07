376c42441c61e96d403019675efbab4d948fedcd
Alix Hui <th.hui2002@gmail.com>
2023-08-07 20:04:07 +0800

    correct API fetch location in production

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import { staticContents } from '../app/staticContent.js'
+import { useRouter } from 'next/router'
 
 export const getStaticProps = async () => {
-  const res = await fetch('http://localhost:3000/api/posts')
-  var posts;
+  var res, posts;
+  const isProd = process.env.NODE_ENV === 'production';
+  if (isProd) {
+    res = await fetch('http://cms-arcannite.vercel.app/api/posts')
+  }
+  else {
+    res = await fetch('http://localhost:3000/api/posts')
+  }
+  
   if (res.status == 200 || res.status == 201) {
     posts = await res.json()
   }
@@ -13,6 +21,7 @@ export const getStaticProps = async () => {
 }
 
 export default function Home({ posts }) {
+  const router = useRouter()
   return (
     <div className="min-h-screen items-center justify-between p-24">
       {
@@ -22,6 +31,7 @@ export default function Home({ posts }) {
             <div> {post.author} </div>
             <div> {post.postDate.slice(0, -1).split("T").join(" ")} </div>
             <div className="text-base/loose"> {post.content} </div>
+            <br/>
           </div>
         ))
       }
